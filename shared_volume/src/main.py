0dc7284221d620d5332a3e9a96540685ee4214bf
Roland Allart <roland.allart1@gmail.com>
2021-10-23 15:07:27 +0200

    Add pdb

--- shared_volume/src/main.py
+++ shared_volume/src/main.py
@@ -1,4 +1,7 @@
 import nltk
+import pdb
+
+ 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
@@ -6,4 +9,11 @@ sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good.
 tokens = nltk.word_tokenize(sentence)
 print(tokens)
 tagged = nltk.pos_tag(tokens)
+
+pdb.set_trace()
 print(tagged[0:6])
+
+test = 42
+pdb.set_trace()
+
+print(test)
